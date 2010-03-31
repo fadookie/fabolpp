80e9278303c93d017b16ae08229a2a81ce7410c8
Eliot Lash <eliot@eliot.bambi.net>
2010-03-31 04:39:00 -0700

    Moved UInt12 code to its own class in fabolpp.cpp.

--- fabolpp.cpp
+++ fabolpp.cpp
@@ -17,25 +17,83 @@
  */
 
 #include <iostream>
+//#include <string>
+#define UINT12MAX 4096
 
 using namespace std;
 typedef unsigned short UINT12;
+//const UINT12 UInt12_max = 4096; //The cap for a 12-bit number from 0-4095
+
+class UInt12
+{
+	UINT12 myUINT12; //Private value
+	UINT12 parse (UINT12 incoming); //wraparound check method
+	public:
+		UInt12();
+		UInt12(UINT12 initialValue);
+		UINT12 set (UINT12 incoming); //Setter
+		UINT12 get () { return myUINT12; }; //getter
+};
+
+UInt12::UInt12 ()
+{
+	myUINT12 = 0;
+}
+
+UInt12::UInt12 (UINT12 initialValue)
+{
+	//InitialValue is a reference, do not modify it!
+	myUINT12 = initialValue;
+}
+
+UINT12 UInt12::set (UINT12 incoming)
+{
+	cout << "\nPre-wrap value: " << incoming;
+	while (incoming >= UINT12MAX) //If myUInt12 goes over 4095, wrap value starting at 0
+	{
+		incoming = incoming - UINT12MAX;
+	}
+	myUINT12 = incoming;
+	cout << "\nPost-wrap value: " << myUINT12 << endl;
+	//cout << "Size: " << sizeof(myUInt12) << " Bytes." << endl;
+}
 
 int main()
 {
-	UINT12 myUInt12 = 4094; //very high 12-bit number
-	const UINT12 UInt12_max = 4096; //The cap for a 12-bit number from 0-4095
-	cout << "myUInt12 is " << myUInt12 << ".\nIncrement by what value? ";
-	UINT12 incrementor;
-	cin >> incrementor;
-	cout << "(UINT12)incrementor == " << (UINT12)incrementor;
-	myUInt12 = myUInt12 + (UINT12)incrementor;
-	cout << "\nPre-wrap value: " << myUInt12;
-	while (myUInt12 >= UInt12_max) //If myUInt12 goes over 4095, wrap value starting at 0
+	UInt12 testUInt12 = 4093; //very high 12-bit number
+	//cout << testUInt12.get() << endl;
+	//testUInt12 = 4094;
+	//cout << testUInt12.get();
+	//testUInt12++;
+	//cout << testUInt12.get();
+	bool execute = true;
+	int incrementor;
+	//string input;
+	
+	cout << "FABOL++ Interpreter Pre-app\n type q to quit.\n";
+
+	while (execute)
 	{
-		myUInt12 = myUInt12 - UInt12_max;
+		cout << "testUInt12 is " << testUInt12.get() << ".\nIncrement by what value? ";
+		cin >> incrementor;
+		//incrementor = (int)input;
+		/*if (incrementor == (int)"q")
+		*{
+		*	cout << "(int)\"q\" = " << (int)"q";
+		*	execute = false;
+		}*/
+		if (incrementor >= 0)
+		{
+			cout << "(UINT12)incrementor == " << (UINT12)incrementor;
+			testUInt12.set(testUInt12.get() + (UINT12)incrementor);
+		}
+		else if (incrementor < 0) //if incrementor is negative
+		{
+			incrementor *= -1; //flip sign
+			cout << "(UINT12)decrementor == " << (UINT12)incrementor;
+			testUInt12.set(testUInt12.get() - (UINT12)incrementor);//and subtract
+		}
+		
 	}
-	cout << "\nPost-wrap value: " << myUInt12 << endl;
-	//cout << "Size: " << sizeof(myUInt12) << " Bytes." << endl;
 	return 0;
 }
